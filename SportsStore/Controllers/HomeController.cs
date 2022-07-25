d72459373eb19b45dbe32f42359c2bb196aaa725
Dora <107618912+doraITman@users.noreply.github.com>
2022-07-25 23:18:16 +0300

    Add project files.

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
